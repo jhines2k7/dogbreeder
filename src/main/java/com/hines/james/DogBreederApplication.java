3d89e1252865efc3c011e8d4feb8b0bb736f35f9
James Hines <jameshines10@gmail.com>
2016-08-19 21:37:27 -0400

    Initial commit

---
+++ src/main/java/com/hines/james/DogBreederApplication.java
@@ -0,0 +1,12 @@
+package com.hines.james;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogBreederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogBreederApplication.class, args);
+	}
+}
